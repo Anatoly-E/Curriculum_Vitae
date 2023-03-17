2c9b788215004a12b45e470b3e04c6a7cd2d1dee
Anantoly Edush <107484175+Anatoly-E@users.noreply.github.com>
2023-03-17 15:25:38 +0300

    Добавил плавную прокрутку страницы и <progress> вверху.

---
+++ script.js
@@ -0,0 +1,28 @@
+// Управляем значением value progress
+
+window.addEventListener("scroll", showProgressValue);
+
+function showProgressValue() {
+  let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrolled = (scroll / height) * 100;
+
+  document.querySelector("#page_progress").value = scrolled;
+}
+
+// Создаем плавную прокрутку по ссылке вверх или вниз
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    const blockId = anchor.getAttribute("href");
+    document.querySelector("" + blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
