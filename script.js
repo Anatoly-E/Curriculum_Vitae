51e976ac457aa7a7546e77bf79415d72a806b327
Anantoly Edush <107484175+Anatoly-E@users.noreply.github.com>
2023-03-23 19:02:04 +0300

    Оживил текст приветствия, навел порядок в JS, сделал валидацию index.html. Добавил gitignore.

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-// Управляю значением value тэга <progress>
+// Значение value тэга <progress>
 
-function showProgressValue() {
+const showProgressValue = () => {
   let scroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let scrolled = (scroll / height) * 100;
-
   document.querySelector("#page_progress").value = scrolled;
-}
+};
 
-window.addEventListener("scroll", showProgressValue);
-
-// Создаю плавную прокрутку по ссылке вверх или вниз
+// Плавная прокрутка по ссылке вверх или вниз
 
 const anchors = document.querySelectorAll('a[href*="#"]');
-
 for (let anchor of anchors) {
-  anchor.addEventListener("click", (event) => {
+  anchor.addEventListener("click", event => {
     event.preventDefault();
     const blockId = anchor.getAttribute("href");
     document.querySelector("" + blockId).scrollIntoView({
@@ -27,44 +21,56 @@ for (let anchor of anchors) {
   });
 }
 
-// Добавляю отклик элементам при скролле
+// Анимация текста приветствия при загрузке страницы
+
+const loadAnimationText = () => {
+  const hello = document.querySelector(".greeting-text h1");
+  const helloName = document.querySelector(".greeting-text h2");
+  console.log(hello);
+  setTimeout(() => {
+    hello.classList.add("hello-show");
+    helloName.classList.add("hello-name-show");
+  }, 1000);
+};
+
+// Анимация появления картинок
+
+const imgContainer = document.querySelectorAll(".img-container");
+const scrollAnimationImg = () => {
+  imgContainer.forEach(elem => {
+    let windowCenter = window.innerHeight / 1.5 + window.scrollY;
+    let itemTop = elem.offsetTop;
+
+    if (windowCenter >= itemTop) {
+      elem.children[0].classList.add("img-show");
+    }
+  });
+};
+
+// Анимация иконок контактов
+
+const contacts = document.querySelector(".contacts");
+const scrollAnimationContacts = () => {
+  let windowBottom = window.innerHeight + window.scrollY;
+  let itemTop = contacts.offsetTop;
+  if (windowBottom >= itemTop) {
+    contacts.children[0].classList.add("icon-telegram-show");
+    contacts.children[1].classList.add("icon-github-show");
+    contacts.children[2].classList.add("icon-vk-show");
+  } else {
+    contacts.children[0].classList.remove("icon-telegram-show");
+    contacts.children[1].classList.remove("icon-github-show");
+    contacts.children[2].classList.remove("icon-vk-show");
+  }
+};
+
+// Добавляю отклик элементам после загрузки страницы
 
 document.addEventListener("DOMContentLoaded", () => {
-  /** Появление картинок*/
-  const imgContainer = document.querySelectorAll(".img-container");
-  const scrollAnimationImg = () => {
-    imgContainer.forEach((elem) => {
-      let windowCenter = window.innerHeight / 1.5 + window.scrollY;
-      let itemTop = elem.offsetTop;
-
-      if (windowCenter >= itemTop) {
-        elem.children[0].classList.add("img-show");
-      }
-    });
-  };
-
-  /** Появление текста под картинками*/
-
-  /** Анимация иконок контактов*/
-  const contacts = document.querySelector(".contacts");
-  const scrollAnimationContacts = () => {
-    let windowBottom = window.innerHeight + window.scrollY;
-    let itemTop = contacts.offsetTop;
-    console.log(contacts.children[0]);
-    if (windowBottom >= itemTop) {
-      contacts.children[0].classList.add("icon-telegram-show");
-      contacts.children[1].classList.add("icon-github-show");
-      contacts.children[2].classList.add("icon-vk-show");
-    } else {
-      contacts.children[0].classList.remove("icon-telegram-show");
-      contacts.children[1].classList.remove("icon-github-show");
-      contacts.children[2].classList.remove("icon-vk-show");
-    }
-  };
-
-  scrollAnimationImg();
-  scrollAnimationContacts();
+  showProgressValue();
+  loadAnimationText();
   window.addEventListener("scroll", () => {
+    showProgressValue();
     scrollAnimationImg();
     scrollAnimationContacts();
   });
